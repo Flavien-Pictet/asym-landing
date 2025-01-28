6846b436caa497bbfd445b48e916fe8f880f8ac4
Picsou <flavien@pictet.net>
2025-01-27 21:04:39 -0800

    push

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -134,7 +134,7 @@ export default function Portfolio() {
                 className="overflow-hidden"
               >
                 <div className="pb-6 text-[#B2B2B2] text-[14px] font-['Rethink_Sans'] font-medium select-none">
-                  <p className="mb-3">Taller is a height prediction app designed for teens under 21 who want to know their futur height & make sure they reach their true height potential. Answer a few lifestyle questions to see your future height and get a personalized plan to optimize your growth.</p>
+                  <p className="mb-3">Answer a few lifestyle questions to see your future height and get a personalized plan to optimize your growth.</p>Taller 
                   <div className="inline-block px-3 py-1" style={{ borderRadius: '6px', border: '1px solid #E4E7EC' }}>
                     <span className="text-[12px] text-black">500k downloads</span>
                   </div>
