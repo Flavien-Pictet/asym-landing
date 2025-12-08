d29e618aacfc18daa9994da35919e5b0087913b2
Picsou <flavien@pictet.net>
2025-12-08 13:24:49 +0400

    Push

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'public/assets/images/admitted-tips/**/*',
-      ],
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
