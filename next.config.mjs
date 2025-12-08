74b1cc62f8d2d0f0a8bf5bb45f10b947ca2dce5e
Picsou <flavien@pictet.net>
2025-12-08 13:16:34 +0400

    j

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'public/assets/images/**/*',
+      ],
+    },
+  },
+};
 
 export default nextConfig;
--- src/app/booksniper/page.js
+++ src/app/booksniper/page.js
@@ -1,6 +1,9 @@
 import { getAllImageSets } from './imageUtils'
 import BooksniperClient from './BooksniperClient'
 
+// Force dynamic rendering to prevent images from being included in build bundle
+export const dynamic = 'force-dynamic'
+
 export default function BooksniperPage() {
   const imageSets = getAllImageSets()
   
